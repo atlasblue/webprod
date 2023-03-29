dfa46d73b3726ba4c1d768117654534f22d09401
atlasblue <92083755+atlasblue@users.noreply.github.com>
2023-03-29 11:52:05 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,6 +8,5 @@ sed -i s\/^SELINUX=.*$\/SELINUX=disabled\/ \/etc\/selinux\/config
 sudo yum install git -y
 git clone https://github.com/atlasblue/webprod.git 
 mv -f webprod/* /usr/share/nginx/html
-ip=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
-sed -i "s/x.x.x.x/$ip/g" /usr/share/nginx/html/index.html
+
 
