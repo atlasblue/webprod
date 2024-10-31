93cfd56ee3f328d1fd78eb8e7ea680e8fff5b2fc
atlasblue <92083755+atlasblue@users.noreply.github.com>
2024-10-31 09:47:41 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,57 +1,23 @@
 #!/bin/bash
 
-#================#
-# For CentOS 7.0 #
-#================#
-
-
-######## CONFIG ##########
-
-user="user"
-router="index.php"
-serverRoot="/usr/share/nginx/html"
-domain="localhost"
-timezone="America/Phoenix"
-
-##########################
-hostnamectl set-hostname vm-prod
-useradd $user
-
-# Repos
-rpm -Uvh https://mirror.webtatic.com/yum/el7/epel-release.rpm
-rpm -Uvh http://yum.postgresql.org/9.3/redhat/rhel-7-x86_64/pgdg-centos93-9.3-1.noarch.rpm
-rpm -Uvh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
-rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
-
-# Installation
-yum install rsync nginx php56w-fpm php56w-gd php56w-mbstring php56w-pecl-xdebug php56w-pgsql php56w-bcmath php56w-opcache php56w-mcrypt php56w wget -y
-
-#Configuration
-su - postgres -c /usr/pgsql-9.3/bin/initdb
-
-#php-fpm configuration
-sed -i "s@;date.timezone =@date.timezone = $timezone@g" /etc/php.ini
-sed -i "s@display_errors = Off@display_errors = On@g" /etc/php.ini
-sed -i "s@html_errors = On@html_errors = Off@g" /etc/php.ini
-sed -i "s@;catch_workers_output =@catch_workers_output =@g" /etc/php-fpm.d/www.conf
-sed -i "s@;php_flag\[display_errors\] = off@php_flag\[display_errors\] = on@" /etc/php-fpm.d/www.conf
-
-# permissions
-sed -i "s@user  nginx@user  $user@g" /etc/nginx/nginx.conf
+#===================#
+# For ROCKY Linux 9 #
+#===================#
+sudo dnf install nginx php php-fpm wget -y
+sudo systemctl enable nginx
+sudo systemctl start nginx
+sudo systemctl enable php-fpm
+sudo systemctl start php-fpm
+user="nginx"
 sed -i "s@user = apache@user = $user@g" /etc/php-fpm.d/www.conf
 sed -i "s@group = apache@group = $user@g" /etc/php-fpm.d/www.conf
-sed -i "s@group = apache@group = $user@g" /etc/php-fpm.d/www.conf
-sed -i "s@SELINUX=enforcing@SELINUX=disabled@g" /etc/sysconfig/selinux
-systemctl stop firewalld
-systemctl disable firewalld
-setenforce 0
-
+sudo systemctl restart php-fpm
+serverRoot="/usr/share/nginx/html"
 wget https://raw.githubusercontent.com/atlasblue/webprod/master/index.php -P $serverRoot
 wget https://raw.githubusercontent.com/atlasblue/webprod/master/prod.png -P $serverRoot
-chown $user /home/$user -R
-chgrp $user /home/$user -R
-
 rm -f /etc/nginx/conf.d/*
+router="index.php"
+domain="localhost"
 echo "
 server {
     listen       80;
@@ -69,12 +35,3 @@ server {
     }
 }
 " > /etc/nginx/conf.d/server.conf
-
-
-# start at boot
-systemctl enable php-fpm
-systemctl enable nginx
-
-# run them
-systemctl start php-fpm
-systemctl start nginx
