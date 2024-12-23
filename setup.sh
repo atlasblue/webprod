6af2cf4961d165495c3f92a8e7efe7e98c5c1641
atlasblue <92083755+atlasblue@users.noreply.github.com>
2024-12-23 22:50:45 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 #===================#
 # For ROCKY Linux 9 #
 #===================#
+
 sudo dnf install nginx php php-fpm wget -y
 sudo systemctl enable nginx
 sudo systemctl start nginx
@@ -15,23 +16,3 @@ sudo systemctl restart php-fpm
 serverRoot="/usr/share/nginx/html"
 wget https://raw.githubusercontent.com/atlasblue/webprod/master/index.php -P $serverRoot
 wget https://raw.githubusercontent.com/atlasblue/webprod/master/prod.png -P $serverRoot
-rm -f /etc/nginx/conf.d/*
-router="index.php"
-domain="localhost"
-echo "
-server {
-    listen       80;
-    server_name  l localhost $domain www.$domain;
-    root $serverRoot;
-    index $router;
-    location / {
-        try_files \$uri \$uri/ /$router;
-    }
-    location ~ \.php$ {
-        fastcgi_index   $router;
-        fastcgi_pass    127.0.0.1:9000;
-        include         fastcgi_params;
-        fastcgi_param   SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-    }
-}
-" > /etc/nginx/conf.d/server.conf
