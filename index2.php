e829f07116d24c9411d4a9f389454b9d8e4397d6
atlasblue <92083755+atlasblue@users.noreply.github.com>
2025-04-08 08:34:42 +0200

    Update index2.php

--- index2.php
+++ index2.php
@@ -1,1 +1,10 @@
-
+<p>&nbsp;</p>
+<p><span style="font-size:36px"><span style="font-family:Arial,Helvetica,sans-serif"><strong>Welcome to the Production environment</strong></span></span></p>
+<p>&nbsp;</p>
+<?php 
+$ip_server = $_SERVER['SERVER_ADDR']; 
+echo '<span style="font-size:36px"><span style="font-family:Arial,Helvetica,sans-serif"><strong>';
+echo "Server IP Address is: $ip_server" ;
+echo "This is Webserver 2" ;
+?> 
+<p><img src="prod.png" /></p>
