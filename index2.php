56afc9515859b543fcf32708e10f905ebb5251eb
atlasblue <92083755+atlasblue@users.noreply.github.com>
2025-04-14 11:16:44 +0200

    Update index2.php

--- index2.php
+++ index2.php
@@ -5,6 +5,6 @@
 $ip_server = $_SERVER['SERVER_ADDR']; 
 echo '<span style="font-size:36px"><span style="font-family:Arial,Helvetica,sans-serif"><strong>';
 echo "Server IP Address is: $ip_server" ;
-echo "<p></p>This is Webserver 2" ;
+echo "<p></p>This is WebProd 2" ;
 ?> 
 <p><img src="prod.png" /></p>
